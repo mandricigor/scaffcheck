98fd360a96a959987bd45075ae527ca79871d76f
Igor Mandric <mandric.igor@gmail.com>
2016-10-24 00:36:24 -0400

    added length and distances to .scaf files

--- build_scaffolds.py
+++ build_scaffolds.py
@@ -23,33 +23,42 @@ a = map(lambda x: x.strip().split(), a)
 
 # groups is the list of groups by reference
 groups = [[a[0]]]
+linegroups = [[0]]
 for i in range(1, len(a)):
     if a[i][11] == groups[-1][-1][11]:
         groups[-1].append(a[i])
+        linegroups[-1].append(i)
     else:
         groups.append([a[i]])
+        linegroups.append([i])
 
 
 # because we may have that some contigs 
 allgroups = []
-for group in groups:
+alllinegroups = []
+for linegroup, group in zip(linegroups, groups):
     group2 = [[group[0]]]
+    linegroup2 = [[linegroup[0]]]
     for j in range(1, len(group)):
         if group[j][12] == group2[-1][-1][12]:
             group2[-1].append(group[j])
+            linegroup2[-1].append(linegroup[j])
         else:
             group2.append([group[j]])
+            linegroup2.append([linegroup[j]])
     allgroups.append(group2)
-
+    alllinegroups.append(linegroup2)
 
 
 contig_map = {}
 global_counter = 0
 
 scaffolds = []
-for group in allgroups:
+chosen_lines = []
+for alllinegroup, group in zip(alllinegroups, allgroups):
     scaf = []
-    for group2 in group:
+    chosen = []
+    for linegroup2, group2 in zip(alllinegroup, group):
         if len(group2) == 1 and len(group2[0]) == 12:
             # if the contig does not match completely -> skip it
             # we do not need it anymore
@@ -63,7 +72,8 @@ for group in allgroups:
                     global_counter += 1
                     name = "contig_%s" % global_counter
                     contig_map[group2[0][12]] = name
-                scaf.append(name + ":::" + group2[0][10])
+                scaf.append(name + ":::" + group2[0][10] + ":::" + str(int(group2[0][1]) - int(group2[0][0])))
+                chosen.append(linegroup2)
         else:
             # here we may have contigs corresponding to circular genomes
             # if it has both [BEGINS] and [ENDS] -> we just split it
@@ -75,8 +85,18 @@ for group in allgroups:
                     global_counter += 1
                     name = "contig_%s" % global_counter
                     contig_map[group2[0][12]] = name
-                scaf.append(name + ":::" + group2[0][10])
+                scaf.append(name + ":::" + group2[0][10] + ":::" + str(int(group2[-1][1]) - int(group2[0][0])))
+                chosen.append(linegroup2)
+    chosen_lines.append(chosen)
     scaffolds.append(scaf)
+
+
+distances = []
+for cluster in chosen_lines:
+    dist = [0]
+    for group_number in range(len(cluster) - 2, -1, -1):
+        dist.insert(0, int(a[cluster[group_number + 1][0]][0]) - int(a[cluster[group_number][-1]][1]))
+    distances.append(dist)
 
 
 rev_contig_map = {}
@@ -86,10 +106,10 @@ for x, y in contig_map.items():
 
 with open("scaffolds.igor.scaf", "w") as f:
     counter = 1
-    for scaffold in scaffolds:
+    for dist, scaffold in zip(distances, scaffolds):
         f.write(">scaffold_%s\n" % counter)
-        for scaf in scaffold:
-            f.write("%s\n" % scaf)
+        for d, scaf in zip(dist, scaffold):
+            f.write("%s:::%s\n" % (scaf, d))
         counter += 1
 
 
